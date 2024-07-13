061fe25187fc8e81e5237178c9b6204cf0d4033b
Ömer Duran <me@omerduran.dev>
2024-07-13 14:15:23 +0200

    iostream problem solved

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
-#include <iosstream>
+#include <iostream>
 #include <omp.h>
-#include <sstream>
 
 int main() {
-#pragma omp parallel
-  {
-    int thread_id = omp_get_thread_num();
-    std::cout << "Hello from thread " << thread_id << std::endl;
-  }
-  return 0;
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        
+        #pragma omp critical
+        std::cout << "Hello from thread " << thread_id << std::endl;
+    }
+    
+    return 0;
 }
