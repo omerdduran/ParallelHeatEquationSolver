381ac4a143f7ad9d63684f3343f9e8a9b7e55d52
Ömer Duran <me@omerduran.dev>
2024-07-25 23:21:14 +0300

    make file

---
+++ include/heat_solver.h
@@ -0,0 +1,8 @@
+//
+// Created by Ömer Duran on 25.07.2024.
+//
+
+#ifndef HEAT_SOLVER_H
+#define HEAT_SOLVER_H
+
+#endif //HEAT_SOLVER_H
---
+++ src/heat_solver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ömer Duran on 25.07.2024.
+//
+
+#include "heat_solver.h"
