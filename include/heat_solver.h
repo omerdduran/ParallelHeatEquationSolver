658ec0cab60f55b7d34291bb3e520374199db79f
Ömer Duran <me@omerduran.dev>
2024-07-26 12:59:53 +0300

    heat_solver.h

--- include/heat_solver.h
+++ include/heat_solver.h
@@ -1,8 +1,10 @@
-//
-// Created by Ömer Duran on 25.07.2024.
-//
-
 #ifndef HEAT_SOLVER_H
 #define HEAT_SOLVER_H
 
-#endif //HEAT_SOLVER_H
+#include <vector>
+
+void initialize(std::vector<double>& u, int nx);
+void solve_serial(std::vector<double>& u, int nx, double alpha, double dt, double dx, int nt);
+void solve_parallel(std::vector<double>& u, int nx, double alpha, double dt, double dx, int nt);
+
+#endif // HEAT_SOLVER_H
